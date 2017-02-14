aaebbdedcbe9a261ada9821550f0617d3783b763
WS <a739002611@outlook.com>
2017-02-14 13:44:38 +0800

    尝试cocoapod 私有仓库

---
+++ wsHelloWorld/Tool.h
@@ -0,0 +1,15 @@
+//
+//  Tool.h
+//  helloWorld
+//
+//  Created by WS on 2017/2/13.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tool : NSObject
+
++ (void)sayHello;
+
+@end
---
+++ wsHelloWorld/Tool.m
@@ -0,0 +1,16 @@
+ //
+//  Tool.m
+//  helloWorld
+//
+//  Created by WS on 2017/2/13.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import "Tool.h"
+
+@implementation Tool
+
++ (void)sayHello{
+    NSLog(@"hello world");
+}
+@end
