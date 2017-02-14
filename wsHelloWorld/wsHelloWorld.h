5515c987770a510b17b9cc38d232e5887cf2ff89
WS <a739002611@outlook.com>
2017-02-14 13:43:50 +0800

    Initial Commit

---
+++ wsHelloWorld/wsHelloWorld.h
@@ -0,0 +1,19 @@
+//
+//  wsHelloWorld.h
+//  wsHelloWorld
+//
+//  Created by WS on 2017/2/14.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wsHelloWorld.
+FOUNDATION_EXPORT double wsHelloWorldVersionNumber;
+
+//! Project version string for wsHelloWorld.
+FOUNDATION_EXPORT const unsigned char wsHelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wsHelloWorld/PublicHeader.h>
+
+
